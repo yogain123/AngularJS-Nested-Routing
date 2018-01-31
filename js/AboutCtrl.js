d9c7cc3b3b67837aadd37ee5008e8867cc7ee5de
yogendra saxena <yogendrasaxena56@gmail.com>
2018-02-01 00:03:39 +0530

    Few changes

--- js/AboutCtrl.js
+++ js/AboutCtrl.js
@@ -2,4 +2,10 @@ app.controller("AboutCtrl", function($scope, $state) {
 
   console.log("inside About Controller controller");
 
+  $scope.items = [{"firstName":"Yogendra","lastName":"Saxena","email":"yogendrasaxena56@gmail.com"},
+{"firstName":"Yogendra","lastName":"Saxena","email":"yogendrasaxena56@gmail.com"},
+{"firstName":"Yogendra","lastName":"Saxena","email":"yogendrasaxena56@gmail.com"},
+{"firstName":"Yogendra","lastName":"Saxena","email":"yogendrasaxena56@gmail.com"},
+{"firstName":"Yogendra","lastName":"Saxena","email":"yogendrasaxena56@gmail.com"},
+{"firstName":"Yogendra","lastName":"Saxena","email":"yogendrasaxena56@gmail.com"}];
 });
--- js/LoginCTRL.js
+++ js/LoginCTRL.js
@@ -2,16 +2,29 @@ app.controller("LoginCtrl", function($scope, $state) {
 
   console.log("inside Login Controller controller");
 
-  $scope.headerCheck = false;
-
+  $scope.submitted = false;
 
 
   $scope.login = function(){
   console.log("inside login function");
+  console.log($scope.username + " "+ $scope.password);
+  if($scope.username==undefined || $scope.password==undefined){
+      $scope.submitted=true;
+    }
 
+  else if($scope.username=="shadowfax" && $scope.password=="12345")
+  {
   $state.go('/home', {
      reload: false
    });
+ }
+ else{
+   console.log("invaid Credentials");
+   $('#modal').modal({backdrop: 'static'});
+	 $('#modal').modal({keyboard: false});
+	 $('#modal').modal('show');
+   return;
+ }
 
   };
 });
